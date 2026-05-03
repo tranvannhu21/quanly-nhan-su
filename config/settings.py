9e377bf1b865a3c0c8f8929e484d773a1be2ab04
Tran Van Nhu <dtc235200572@ictu.edu.vn>
2026-05-03 17:13:29 +0700

    Fix loi ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^a7a(%cvl!f)yn^$3!ac2ydv&u&!9f&+upl@(%0x&+hzwpnety
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
